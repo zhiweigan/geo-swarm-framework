a732835bd31dfd4724fb6dd44ef9ad947476ebc9
Zhi Wei Gan <zgan@mit.edu>
2023-02-22 08:44:47 -0500

    constants added

--- apps/alloc/constants.h
+++ apps/alloc/constants.h
@@ -1,12 +1,12 @@
-#define HEIGHT 10
-#define WIDTH 10
-#define NUM_AGENTS 50
+#define HEIGHT 1000
+#define WIDTH 1000
+#define NUM_AGENTS 10000000
 #define MAX_ITERS 100
-#define VERBOSE true
+#define VERBOSE false
 
 // NOT EVERY APP
 #define ADD_SPACE_REQ 1
 
 // APP SPECIFIC
-#define NUM_TASKS 20
-#define TOTAL_DEMAND 50
+#define NUM_TASKS 200000
+#define TOTAL_DEMAND 500000
